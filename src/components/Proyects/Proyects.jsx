c1ca38903512fa8b7d20b499e0d4831367e7afca
Marco <marcofernandezc@outlook.com>
2024-07-18 08:36:49 -0300

    proyects

--- src/components/Proyects/Proyects.jsx
+++ src/components/Proyects/Proyects.jsx
@@ -1,18 +1,78 @@
 import React from "react";
 
 import { Name } from "../../pages/Home/HomeStyled";
-import { ContProyectItems, ProyectCont, ProyectItem } from "./ProyectsStyled";
-
+import {
+	ContProyectItems,
+	ProyectCont,
+	ProyectItem,
+	ProyectItemImg,
+	ProyectItemSkillsCont,
+	ProyectItemTitle,
+} from "./ProyectsStyled";
+import Ecommerce from "../../assets/img/E-commerce.png";
+import Api from "../../assets/img/Api-back.png";
+import Tateti from "../../assets/img/Tateti.png";
+import Landing from "../../assets/img/Landing.png";
+import { FaHtml5 } from "react-icons/fa";
+import { SiExpress, SiMongodb, SiRedux } from "react-icons/si";
+import { FaCss3Alt, FaNodeJs } from "react-icons/fa6";
+import { RiJavascriptFill } from "react-icons/ri";
+import { BiLogoTypescript } from "react-icons/bi";
+import { IoLogoReact } from "react-icons/io5";
 const Proyects = () => {
 	return (
 		<>
 			<ProyectCont>
 				<Name>Proyects</Name>
 				<ContProyectItems>
-					<ProyectItem>asd</ProyectItem>
-					<ProyectItem>asd</ProyectItem>
-					<ProyectItem>asd</ProyectItem>
-					<ProyectItem>asd</ProyectItem>
+					<ProyectItem>
+						<a href="https://integrador-react-iota.vercel.app/">
+							<ProyectItemImg src={Ecommerce} alt="E-commerce" />
+						</a>
+						<ProyectItemTitle>E-commerce</ProyectItemTitle>
+						<ProyectItemSkillsCont>
+							<FaHtml5 />
+							<FaCss3Alt />
+							<RiJavascriptFill />
+							<IoLogoReact />
+							<SiRedux />
+						</ProyectItemSkillsCont>
+					</ProyectItem>
+					<ProyectItem>
+						<a href="https://api-back-six.vercel.app/">
+							<ProyectItemImg src={Api} alt="Api (e-commerce)" />
+						</a>
+						<ProyectItemTitle>Api e-commerce</ProyectItemTitle>
+						<ProyectItemSkillsCont>
+							<FaHtml5 />
+							<BiLogoTypescript />
+							<FaNodeJs />
+							<SiExpress />
+							<SiMongodb />
+						</ProyectItemSkillsCont>
+					</ProyectItem>
+					<ProyectItem>
+						<a href="https://ta-te-ti-orcin-rho.vercel.app/">
+							<ProyectItemImg src={Tateti} alt="tateti" />
+						</a>
+						<ProyectItemTitle>Ta-Te-Ti</ProyectItemTitle>
+						<ProyectItemSkillsCont>
+							<FaHtml5 />
+							<FaCss3Alt />
+							<RiJavascriptFill />
+							<IoLogoReact />
+						</ProyectItemSkillsCont>
+					</ProyectItem>
+					<ProyectItem>
+						<a href="https://pagina-seven.vercel.app/">
+							<ProyectItemImg src={Landing} alt="landing" />
+						</a>
+						<ProyectItemTitle>Landing</ProyectItemTitle>
+						<ProyectItemSkillsCont>
+							<FaHtml5 />
+							<FaCss3Alt />
+						</ProyectItemSkillsCont>
+					</ProyectItem>
 				</ContProyectItems>
 			</ProyectCont>
 		</>
--- src/components/Proyects/ProyectsStyled.js
+++ src/components/Proyects/ProyectsStyled.js
@@ -17,12 +17,37 @@ export const ContProyectItems = styled.div`
 	display: flex;
 	align-items: center;
 	justify-content: center;
-	gap: 20px;
+	gap: 30px;
+	flex-wrap: wrap;
 `;
 
 export const ProyectItem = styled.div`
 	display: flex;
-	justify-content: center;
+	justify-content: space-between;
 	flex-direction: column;
 	align-items: center;
+	width: 400px;
+	height: 350px;
+	background-color: var(--secondbg);
+	border-radius: 10px;
+	padding: 20px 10px;
+	gap: 7px;
+`;
+
+export const ProyectItemImg = styled.img`
+	width: 100%;
+	border-radius: 10px;
+`;
+
+export const ProyectItemTitle = styled.h3`
+	color: wheat;
+	font-size: 22px;
+`;
+
+export const ProyectItemSkillsCont = styled.div`
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	font-size: 30px;
+	gap: 5px;
 `;
